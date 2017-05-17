3eb55f4bcc3153d0eef54ec960cda6f8cd6e336a
ivajlotokiew <ivajlo_nick@abv.bg>
2017-05-17 23:31:45 +0300

    Task

---
+++ Cars/Cars.Models/Car.cs
@@ -0,0 +1,20 @@
+﻿namespace Cars.Models
+{
+    using System.Collections.Generic;
+
+    public class Car
+    {
+        private ICollection<User> owners;
+
+        public Car()
+        {
+            this.owners = new HashSet<User>();
+        }
+
+        public string Model { get; set; }
+
+        public decimal Price { get; set; }
+
+        public virtual ICollection<User> Owners { get; set; }
+    }
+}
---
+++ Cars/Cars.Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Cars.Models
+{
+    using System.Collections.Generic;
+
+    public class User
+    {
+        private ICollection<Car> cars;
+
+        public User()
+        {
+            this.cars = new HashSet<Car>();
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public virtual ICollection<Car> Cars { get; set; }
+    }
+}
